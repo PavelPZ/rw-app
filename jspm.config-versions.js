fb7ea7ea9167309aece3042a73c1c45db6fa2ed8
PZ <pzika@langmaster.cz>
2016-11-06 20:06:38 +0100

    v

--- jspm.config-versions.js
+++ jspm.config-versions.js
@@ -6,15 +6,19 @@ SystemJS.config({
       "npm:react-dom@15.3.2": "jspm_packages/npm/react-dom@15.3.2/dist/react-dom.min.js",
       "npm:": "/jspm_packages/npm/",
       "github:": "/jspm_packages/github/",
-      "local:": "/jspm_packages/local/"
+      "local:": "/jspm_packages/local/",
+      "globalize-runtime": "/jspm_packages/npm/globalize@1.1.1/dist/globalize-runtime.js",
+      "globalize-runtime/*": "/jspm_packages/npm/globalize@1.1.1/dist/globalize-runtime/*",
     }
   },
   browserConfig: {
     "paths": {
       "npm:": "/jspm_packages/npm/",
       "github:": "/jspm_packages/github/",
-      "local:": "/jspm_packages/local/"
-    }
+      "local:": "/jspm_packages/local/",
+      "globalize-runtime": "/jspm_packages/npm/globalize@1.1.1/dist/globalize-runtime.js",
+      "globalize-runtime/*": "/jspm_packages/npm/globalize@1.1.1/dist/globalize-runtime/*",
+      }
   },
 });
 
--- jspm.config.js
+++ jspm.config.js
@@ -1,11 +1,15 @@
 SystemJS.config({
+
   browserConfig: {
-    "paths": {
+    production: true,
+    paths: {
+      "npm:react@15.3.2": "jspm_packages/npm/react@15.3.2/dist/react.min.js",
+      "npm:react-dom@15.3.2": "jspm_packages/npm/react-dom@15.3.2/dist/react-dom.min.js",
       "npm:": "/jspm_packages/npm/",
       "github:": "/jspm_packages/github/",
       "local:": "/jspm_packages/local/",
       "globalize-runtime": "/jspm_packages/npm/globalize@1.1.1/dist/globalize-runtime.js",
-      "globalize-runtime/*": "/jspm_packages/npm/globalize@1.1.1/dist/globalize-runtime/*"
+      "globalize-runtime/*": "/jspm_packages/npm/globalize@1.1.1/dist/globalize-runtime/*",
     }
   },
   nodeConfig: {
@@ -14,7 +18,18 @@ SystemJS.config({
       "github:": "jspm_packages/github/",
       "local:": "jspm_packages/local/"
     }
+  },
+
+  production: true,
+  "paths": {
+    "globalize-runtime": "/jspm_packages/npm/globalize@1.1.1/dist/globalize-runtime.js",
+    "globalize-runtime/*": "/jspm_packages/npm/globalize@1.1.1/dist/globalize-runtime/*",
+    "globalize-runtime/number": "/jspm_packages/npm/globalize@1.1.1/dist/globalize-runtime/number.js",
+    "globalize-runtime/date": "/jspm_packages/npm/globalize@1.1.1/dist/globalize-runtime/date.js",
+    "globalize-runtime/plural": "/jspm_packages/npm/globalize@1.1.1/dist/globalize-runtime/plural.js",
+    "globalize-runtime/relative-time": "/jspm_packages/npm/globalize@1.1.1/dist/globalize-runtime/relative-time.js",
   }
+
 });
 
 SystemJS.config({
