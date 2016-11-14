c9ad5337f76766923253fa65e0f2cc35a407431e
PZ <pzika@langmaster.cz>
2016-11-14 16:02:55 +0100

    v

--- tests/login/test.ts
+++
@@ -1,7 +0,0 @@
-﻿import { init as loginInit } from 'rw-lib/login/client/index';
-import { init as navigInit } from 'rw-lib/navig/dispatcher';
-
-export function init() {
-  loginInit();
-  navigInit(null);
-}
